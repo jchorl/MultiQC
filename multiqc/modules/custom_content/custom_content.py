3632a415534f7c35a17998e8ec7ca44868826abc
Josh Chorlton <jchorlton@gmail.com>
2024-07-30 16:48:23 +0000

    fix gs check

--- multiqc/modules/custom_content/custom_content.py
+++ multiqc/modules/custom_content/custom_content.py
@@ -270,7 +270,7 @@ def custom_module_classes() -> List[BaseMultiqcModule]:
 
     # If we only have General Stats columns then there are no module outputs
     if len(sorted_modules) == 0:
-        if len(cust_mod_by_id) == 1 and list(cust_mod_by_id.values())[0]["config"].get("plot_type") == "generalstats":
+        if len(cust_mod_by_id) >= 1 and all(cv["config"].get("plot_type") == "generalstats" for cv in cust_mod_by_id.values()):
             sorted_modules = [bm]
         else:
             raise ModuleNoSamplesFound
